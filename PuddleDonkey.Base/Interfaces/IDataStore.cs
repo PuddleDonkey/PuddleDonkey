0f79b4d52cf4cbd8982a9b9bbf327af0168000d5
Peter Taylor <me@et1.uk>
2023-03-30 21:15:39 +0100

    Added Interfaces.

---
+++ PuddleDonkey.Base/Interfaces/IDataStore.cs
@@ -0,0 +1,24 @@
+﻿namespace PuddleDonkey.Base;
+
+/// <summary>
+/// Interface representing a Data Store, Configuration or Secret.
+/// </summary>
+public interface IDataStore {
+    /// <summary>
+    /// Get the value of the secret for a given key.
+    /// </summary>
+    /// <typeparam name="T">The Type calling the function, Normally a <seealso
+    /// cref="IJobFactory"/> or <seealso cref="IJobArtisan"/></typeparam> <param
+    /// name="key">The Key for the Value</param> <returns>The Stored
+    /// Value</returns>
+    string GetValue<T>(string key);
+
+    /// <summary>
+    /// Sets the value of the secret for a given key.
+    /// </summary>
+    /// <typeparam name="T">The Type calling the function, Normally a <seealso
+    /// cref="IJobFactory"/> or <seealso cref="IJobArtisan"/></typeparam> <param
+    /// name="key">The Key for the Value</param> <param name="value">The Value To
+    /// Set</param>
+    void SetValue<T>(string key, string value);
+}
---
+++ PuddleDonkey.Base/Interfaces/IJobFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace PuddleDonkey.Base;
+
+/// <summary>
+/// Interface for an addon
+/// </summary>
+public interface IJobFactory {
+    /// <summary>
+    /// The configuration required for operation.
+    /// </summary>
+    ICollection<string> RequiredConfigs { get; }
+
+    /// <summary>
+    /// The secrets required for operation.
+    /// </summary>
+    ICollection<string> RequiredSecrets { get; }
+
+    /// <summary>
+    /// Load all the availible jobs, and  add into the manager's queue. Should act
+    /// statelessly as if it were a static method.
+    /// </summary>
+    /// <param name="manager">The job manager object</param>
+    void LoadJobs(IJobManager manager);
+
+    /// <summary>
+    /// The ID of the Job Creator, may be used in the file output path
+    /// </summary>
+    string Id { get; }
+}
